052fe416d13f336fe7ab81b3faa5574b6e470731
Nickolay Grechkin <nickolaygrechkin@gmail.com>
2023-06-16 19:55:27 +0300

    Set up web socket for sending messages between connected clients

--- backend/src/libs/packages/server-application/server-app.ts
+++ backend/src/libs/packages/server-application/server-app.ts
@@ -4,7 +4,7 @@ import { RouteParameters } from "../../../shared/libs/interfaces/routeParameters
 import { HttpMethod } from "../../../shared/libs/enums/httpMethod";
 import { authMiddleware } from "../authMiddleware/authMiddleware";
 import * as dotenv from 'dotenv';
-import * as WebSocket from 'ws';
+import ws, { WebSocketServer } from 'ws';
 import * as http from 'http';
 
 const router = express.Router();
@@ -27,7 +27,7 @@ class ServerApp {
         this.database = database;
         this.api = api;
         this.server = http.createServer(this.app);
-        this.webSocketServer = new WebSocket.Server({ server: this.server });
+        this.webSocketServer = new WebSocketServer({ server: this.server });
         this.clients = [];
     }
 
@@ -54,27 +54,17 @@ class ServerApp {
         this.initMiddlewares();
         this.initRoutes();
 
-        this.webSocketServer.on('connection', (ws: WebSocket) => {
-            let id = Math.random();
-            this.clients[id] = ws;
-
-            const connectedClients = this.clients;
-
-            ws.on('message', function(message) {
-                console.log('получено сообщение ' + message);
-
-                console.log(connectedClients);
-
-                for (let key in connectedClients) {
-                    connectedClients[key].send(message);
+        this.webSocketServer.on('connection', (client) => {
+            console.log('Client connected !')
+            client.on('message', (msg) => {    // (3)
+                console.log(`Message:${msg}`);
+                for (const client of this.webSocketServer.clients) {
+                    if (client.readyState === ws.OPEN) {
+                        client.send(msg)
+                    }
                 }
-            });
-
-            ws.on('close', function() {
-                console.log('соединение закрыто ' + id);
-                delete connectedClients[id];
-            });
-        });
+            })
+        })
 
         this.server.listen(process.env.APP_PORT,() => {
             console.log("Listening on " + process.env.APP_PORT);
