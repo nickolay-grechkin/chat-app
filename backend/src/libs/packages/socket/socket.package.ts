11cb6dfa6dd8229c6c388c71261a87ca692d80e1
Nickolay Grechkin <nickolaygrechkin@gmail.com>
2023-06-18 10:10:19 +0300

    Send dialog id together with message content

--- backend/src/libs/packages/socket/socket.package.ts
+++ backend/src/libs/packages/socket/socket.package.ts
@@ -11,10 +11,11 @@ class Socket {
     public init() {
         this.webSocketServer.on('connection', (client) => {
             console.log('Client connected !')
-            client.on('message', (msg) => {
+            client.on('message', (msg: string) => {
                 for (const client of this.webSocketServer.clients) {
                     if (client.readyState === ws.OPEN) {
-                        client.send(msg)
+                        const parsedRequest = JSON.parse(msg);
+                        client.send(parsedRequest.message);
                     }
                 }
             })
