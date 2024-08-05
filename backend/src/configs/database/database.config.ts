d54a5771330d435e0e0d333a8fd4cba7e19198b0
nickolaygrechkin@gmail.com <nickolaygrechkin@gmail.com>
2024-08-05 16:30:18 +0300

    Revert "Add mysql to test rds db connection"
    This reverts commit 43949f6683743dd5b5c52ca407571512bc38e5b5.

--- backend/src/configs/database/database.config.ts
+++ backend/src/configs/database/database.config.ts
@@ -1,25 +1,10 @@
 import { Model } from "objection";
 import Knex from "knex";
 import { IDatabase } from "../../services/common/interfaces/database.interface";
-import { Client } from "pg";
-import * as mysql from "mysql";
 
 class Database implements IDatabase{
     public connect (): ReturnType<IDatabase['connect']> {
-        var connection = mysql.createConnection({
-            host     : 'chat-app-2.cig7pnqjpyrm.us-east-1.rds.amazonaws.com',
-            user     : 'admin',
-            password : 'chat-app'
-        });
-
-        connection.connect();
-
-        connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-            if (error) throw error;
-            console.log('The solution is: ', results[0].solution);
-        });
-        
-        // Model.knex(Knex(this.initialConfig()));
+        Model.knex(Knex(this.initialConfig()));
     }
 
     public initialConfig() {
@@ -36,11 +21,10 @@ class Database implements IDatabase{
         return {
             client: 'pg',
             connection: {
-                host: 'chat-app.cig7pnqjpyrm.us-east-1.rds.amazonaws.com',
+                host: 'localhost',
                 user: 'postgres',
                 password: 'postgres',
-                port: '5432',
-                database: 'chat-app'
+                database: 'chatapp',
             },
             migrations: {
                 directory: 'src/db/migrations',
